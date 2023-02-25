a34f44daffb66422c9d6ee12f232f4a5e577afce
Aaron Yorks <aaronjyorks@gmail.com>
2023-02-25 12:57:20 -0700

    Second commit

---
+++ memory.js
@@ -0,0 +1,4 @@
+const tilesContainer = document.querySelector(".tiles");
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "gold"];
+const colorsPicklist = [...colors, ...colors];
+console.log(colorsPicklist);
