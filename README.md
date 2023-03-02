e68c4a5c11352a0aae91202de4779b113699138b
Aaron Yorks <aaronjyorks@gmail.com>
2023-03-01 17:38:14 -0700

    Third commit

--- README.md
+++ README.md
@@ -6,3 +6,11 @@ This game just needs to show simple DOM manipulation. Don't over-think it!
 2. You are a solo player: make a scoreboard for yourself
 3. Style your background and create a "face" to your game
 4. Create a basic layout
+5. Use defer to connect to the DOM
+
+# User Story
+    As a user, I want to
+    1. Have my game be interactive
+    2. When I click a button I get a response
+    3. My game resets after I win or lose
+    4. My game keeps track or my wins and losses 
--- memory.js
+++ memory.js
@@ -1,4 +1,96 @@
+//Define your constants
 const tilesContainer = document.querySelector(".tiles");
 const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet", "gold"];
 const colorsPicklist = [...colors, ...colors];
-console.log(colorsPicklist);
+const tileCount = colorsPicklist.length;
+
+//Game state
+let revealedCount = 0;
+let activeTile = null;
+let awaitingEndOfMove = false; //this means that when you click on the tile again the color doesn't disappear
+let totalWins = 0;
+let totalLoses = 0;
+
+
+
+// Makes a div class in JS instead of HTML
+function buildTile(color) {
+    const element = document.createElement("div");
+
+    element.classList.add("tile");
+    element.setAttribute("data-color", color);
+    element.setAttribute("data-revealed", "false");
+// Add an event listener so something happens when you click on a tile
+    element.addEventListener("click", () => {
+        if (awaitingEndOfMove) {
+            return;
+        }
+// the event listener above makes sure that even if you get a move wrong, it doesn't tell you the answer
+        element.style.backgroundColor = color;
+
+        if (!activeTile) {
+            activeTile = element;
+
+            return;
+        }
+        // this above makes it so when you click on one tile, then go click another, the console saves the first tile's info so it can see if they are a match or not
+
+        const colorToMatch = activeTile.getAttribute("data-color");
+
+        if (colorToMatch === color) {
+            activeTile.setAttribute("data-revealed", "true");
+            element.setAttribute("data-revealed", "true");
+
+            awaitingEndOfMove = false; 
+            activeTile = null;
+            revealedCount += 2;
+
+            if (revealedCount === tileCount) {
+                alert("You win! Refresh To Play Again.");
+            }
+
+            return;
+        }
+
+
+
+        // you need to set awaitingEndOfMove to true incase the user got their match wrong. Set a timer to reset the tiles after. 
+        awaitingEndOfMove = true;
+
+        setTimeout(() => {
+            element.style.backgroundColor = null;
+            activeTile.style.backgroundColor = null;
+            awaitingEndOfMove = false; 
+            activeTile = null;
+        }, 1000);
+        // this below will grab and save the second tile's info so we can make a function to see if they match.
+    });
+
+
+    return element;
+}
+
+
+
+//build up tiles using a for loop
+for (let i = 0; i < tileCount; i++) {
+    const randomIndex = Math.floor(Math.random() * colorsPicklist.length);
+    const color = colorsPicklist[randomIndex];
+    const tile = buildTile(color);
+   
+    colorsPicklist.splice(randomIndex, 1);
+    tilesContainer.appendChild(tile);
+    
+}
+
+function getResult () {
+    if (result === won) {
+        return totalWins = totalWins++;
+    } else if (result === lost) {
+            return totalLoses = totalLoses++;
+        }
+        }
+    document.getElementById("wincount").innerHTML = totalWins;
+    document.getElementById("lostcount").innerHTML = totalLoses;
+    console.log(totalWins);
+    console.log(totalLoses);
